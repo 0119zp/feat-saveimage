1190491acf7b422d83b576e0d1dab36cbdfc13c1
张攀攀 <zpzibo0119@163.com>
2019-06-22 17:56:29 +0800

    图片保存&内部存储/外部存储路径比较

--- app/src/main/java/com/example/saveimage/FilePathActivity.java
+++ app/src/main/java/com/example/saveimage/FilePathActivity.java
@@ -1,8 +1,12 @@
 package com.example.saveimage;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.TextView;
+import java.io.File;
 
 /**
  * @author Panpan Zhang
@@ -12,7 +16,9 @@ import android.widget.TextView;
  */
 public class FilePathActivity extends Activity {
 
-    private TextView filePath;
+    private TextView intornalFilePath;
+    private TextView extornalFilePath;
+    private TextView systemFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,59 @@ public class FilePathActivity extends Activity {
     }
 
     private void initView() {
-        filePath = findViewById(R.id.tv_file_path);
+        intornalFilePath = findViewById(R.id.tv_intornal_file_path);
+        extornalFilePath = findViewById(R.id.tv_extornal_file_path);
+        systemFilePath = findViewById(R.id.tv_system_file_path);
+
+        setInternalStorage();
+        setExternalStorage();
+        setSystemStorage();
+    }
+
+    private void setInternalStorage() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("1.Environment.getDataDirectory()\n=").append(Environment.getDataDirectory()).append("\n");
+        sb.append("2.getFilesDir().getAbsolutePath()\n=").append(getFilesDir().getAbsolutePath()).append("\n");
+        sb.append("3.getCacheDir().getAbsolutePath()\n=").append(getCacheDir().getAbsolutePath()).append("\n");
+        sb.append("4.getDir(“saveFile”, MODE_PRIVATE).getAbsolutePath()\n=")
+            .append(getDir("saveFile", MODE_PRIVATE).getAbsolutePath())
+            .append("\n");
+        intornalFilePath.setText(sb.toString());
+    }
+
+    private void setExternalStorage() {
+        // 遍历外部存储路径
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            File[] files = getExternalFilesDirs(Environment.MEDIA_MOUNTED);
+            for (File file : files) {
+                Log.e("zpan", file.getAbsolutePath());
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append("1.Environment.getExternalStorageDirectory().getAbsolutePath()\n=")
+            .append(Environment.getExternalStorageDirectory().getAbsolutePath())
+            .append("\n");
+        sb.append("2.Environment.getExternalStoragePublicDirectory(“”).getAbsolutePath()\n=")
+            .append(Environment.getExternalStoragePublicDirectory("").getAbsolutePath())
+            .append("\n");
+        sb.append("3.getExternalFilesDir(“”).getAbsolutePath()\n=").append(getExternalFilesDir("").getAbsolutePath()).append("\n");
+        sb.append("4.getExternalCacheDir().getAbsolutePath()\n=").append(getExternalCacheDir().getAbsolutePath()).append("\n");
+        extornalFilePath.setText(sb.toString());
+
+        Log.e("zpan", "Environment.getExternalStorageDirectory().getAbsolutePath()");
+        Log.e("zpan", Environment.getExternalStorageDirectory().getAbsolutePath());
+        Log.e("zpan", "Environment.getExternalStoragePublicDirectory(\"\").getAbsolutePath()");
+        Log.e("zpan", Environment.getExternalStoragePublicDirectory("").getAbsolutePath());
+        Log.e("zpan", "getExternalFilesDir(\"\").getAbsolutePath()");
+        Log.e("zpan", getExternalFilesDir("").getAbsolutePath());
+        Log.e("zpan", "getExternalCacheDir().getAbsolutePath()");
+        Log.e("zpan", getExternalCacheDir().getAbsolutePath());
+    }
+
+    private void setSystemStorage() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("1.Environment.getDownloadCacheDirectory()\n=").append(Environment.getDownloadCacheDirectory()).append("\n");
+        sb.append("2.Environment.getRootDirectory()\n=").append(Environment.getRootDirectory()).append("\n");
+        systemFilePath.setText(sb.toString());
     }
 }
--- app/src/main/java/com/example/saveimage/MainActivity.java
+++ app/src/main/java/com/example/saveimage/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.saveimage;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
@@ -10,10 +11,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
@@ -29,6 +32,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private Bitmap saveBitmap;
+    private TextView savePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        savePath = findViewById(R.id.tv_image_save_path);
         AppCompatImageView imageView = findViewById(R.id.iv_save_imageview);
         AppCompatButton saveImage = findViewById(R.id.btn_save_image);
+        AppCompatButton startFilePath = findViewById(R.id.btn_start_file_path);
+        startFilePath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FilePathActivity.class));
+            }
+        });
         saveImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "未获得图片", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                saveImageToAlbum(saveBitmap);
+
+                String[] mPermissionList = new String[] {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE
+                };
+                ActivityCompat.requestPermissions(MainActivity.this, mPermissionList, 100);
             }
         });
 
@@ -65,14 +81,16 @@ public class MainActivity extends AppCompatActivity {
                 saveBitmap = resource;
             }
         });
+
+        savePath.setText(getImageCacheDir().getAbsolutePath());
     }
 
     public void saveImageToAlbum(Bitmap bmp) {
         // 首先保存图片
-        File appDir = getExternalCacheDir(this);
+        File cacheDir = getImageCacheDir();
 
         String fileName = System.currentTimeMillis() + ".jpg";
-        File file = new File(appDir, fileName);
+        File file = new File(cacheDir, fileName);
         try {
             FileOutputStream fos = new FileOutputStream(file);
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
@@ -94,30 +112,42 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         // 最后通知图库更新
-        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(appDir)));
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(cacheDir)));
     }
 
     /**
      * 图片保存路径
      */
-    public File getExternalCacheDir(final Context context) {
-        if (hasExternalCacheDir()) {
-            return context.getExternalFilesDir("");
+    public File getImageCacheDir() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return getExternalFilesDir("");
         }
-
-        final String cacheDir = "/Android/data/" + context.getPackageName() + "/files/";
-        return createFile(Environment.getExternalStorageDirectory().getPath() + cacheDir, "");
-    }
-
-    public boolean hasExternalCacheDir() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+        return createFile(Environment.getExternalStorageDirectory().getAbsolutePath(), "zpan");
     }
 
     public File createFile(String folderPath, String fileName) {
-        File destDir = new File(folderPath);
+        File destDir = new File(folderPath, fileName);
         if (!destDir.exists()) {
             destDir.mkdirs();
         }
-        return new File(folderPath, fileName);
+        return destDir;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100:
+                boolean writeExternalStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readExternalStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (grantResults.length > 0 && writeExternalStorage && readExternalStorage) {
+                    saveImageToAlbum(saveBitmap);
+                } else {
+                    Toast.makeText(this, "请设置存储权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
